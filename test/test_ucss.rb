06ded94d36dff89b1f037d71ad6fd4f55fca2009
Sean Walker <swlkr.rbl@gmail.com>
2021-09-03 12:43:11 -0600

    Split up constant. Generate styles ahead of time

--- test/test_ucss.rb
+++ test/test_ucss.rb
@@ -13,6 +13,13 @@ class UCssTest < Minitest::Test
     FileUtils.rm_rf('./views')
   end
 
+  def test_p_4
+    actual = @ucss.content %w[p-4]
+    expected = '.p-4 { padding: 1rem; }'
+
+    assert_equal expected, actual
+  end
+
   def test_classes
     actual = @ucss.content(%w[p-4 bg-green-500])
     expected = ".p-4 { padding: 1rem; }\n.bg-green-500 { background: #22c55e; }"
@@ -31,6 +38,34 @@ class UCssTest < Minitest::Test
   def test_flex
     expected = ".flex { display: flex; }\n.flex-auto { flex: 1 1 auto; }\n.flex-col { flex-direction: column; }"
     actual = @ucss.content(%w[flex flex-auto flex-col])
+
+    assert_equal expected, actual
+  end
+
+  def test_no_modifier_for_rule_that_exists
+    expected = ''
+    actual = @ucss.content(%w[p])
+
+    assert_equal expected, actual
+  end
+
+  def test_text
+    expected = ".text-green-500 { color: #22c55e; }\n.text-left { text-align: left; }"
+    actual = @ucss.content %w[text-green-500 text-left]
+
+    assert_equal expected, actual
+  end
+
+  def test_flex_align
+    expected = ".justify-evenly { justify-content: space-evenly; }\n.justify-items-center { justify-items: center; }\n.items-center { align-items: center; }"
+    actual = @ucss.content %w[justify-evenly justify-items-center items-center]
+
+    assert_equal expected, actual
+  end
+
+  def test_nested_gap
+    expected = ".gap-y-px { row-gap: 1px; }\n.gap-x-px { column-gap: 1px; }"
+    actual = @ucss.content %w[gap-y-px gap-x-px]
 
     assert_equal expected, actual
   end
